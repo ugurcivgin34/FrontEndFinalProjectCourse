da5881637642635f2c9127b3e80e74f7b92667c4
Uğur Okan Çivgin <37384522+ugurcivgin34@users.noreply.github.com>
2022-02-18 01:01:17 +0300

    .

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {path:"",pathMatch:"full",component:ProductComponent},
+  //router-outlet e denk geliyor.Html kısmında bu prodcut şeklinde güncellensin olarak ayarladık
+  {path:"",pathMatch:"full",component:ProductComponent}, //pathWatch hiç bir şey yoksa onu full olarak algıla ondan sonra ne gelirse ayrı olarak hesapla
   {path:"products",component:ProductComponent},
   {path:"products/category/:categoryId",component:ProductComponent}
 ];
--- src/app/conponents/category/category.component.ts
+++ src/app/conponents/category/category.component.ts
@@ -1,6 +1,9 @@
 import { CategoryService } from './../../services/category.service';
 import { Category } from './../../models/category';
 import { Component, OnInit } from '@angular/core';
+import { ConnectableObservable } from 'rxjs';
+import { CursorError } from '@angular/compiler/src/ml_parser/lexer';
+
 
 @Component({
   selector: 'app-category',
@@ -9,12 +12,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoryComponent implements OnInit {
 
-  categories : Category[];
-  currentCategory:Category;  //"strictPropertyInitialization": false,  newlemedik yani bu yapıyı false yaparak interface in referansından yararlanıyoruz.Yararlamak istediğimiz için bunu fal se yaptık
-//Diğer türlü yapmak istiyorsak ya class şeklinde tanımlayıp newlememiz gerekiyor yada fake properrtilerini tanımlamak gerekiyor
-//angular da kullanacağımız ifadeleri bu şekilde bir yerde tutmamız gerekiyor.Html burdan yürüyor çünkü
+  categories: Category[];
+  currentCategory: Category | undefined;  //"strictPropertyInitialization": false,  newlemedik yani bu yapıyı false yaparak interface in referansından yararlanıyoruz.Yararlamak istediğimiz için bunu fal se yaptık
+  //Diğer türlü yapmak istiyorsak ya class şeklinde tanımlayıp newlememiz gerekiyor yada fake properrtilerini tanımlamak gerekiyor
+  //angular da kullanacağımız ifadeleri bu şekilde bir yerde tutmamız gerekiyor.Html burdan yürüyor çünkü
 
-  constructor(private categoryService:CategoryService) { }
+  constructor(private categoryService: CategoryService) { }
 
   ngOnInit(): void {
     this.getCategories();
@@ -22,30 +25,38 @@ export class CategoryComponent implements OnInit {
 
   getCategories() {
 
-    this.categoryService.getCategories().subscribe(response=>{
-        this.categories=response.data;
+    this.categoryService.getCategories().subscribe(response => {
+      this.categories = response.data;
 
     });
   }
 
-  setCurrentCategory(category:Category){
-      this.currentCategory=category;
+  setCurrentCategory(category: Category) {
+    this.currentCategory = category;
   }
 
-  getCurrentCategoryClass(category:Category){
-    if (category==this.currentCategory) {
+
+  getCurrentCategoryClass(category: Category) {
+    if (category == this.currentCategory) {
+
       return "list-group-item active"
     } else {
+
       return "list-group-item"
     }
   }
 
-  getAllCategoryClass(){
+  getAllCategoryClass() {
     if (!this.currentCategory) {
+
       return "list-group-item active"
     } else {
+
       return "list-group-item"
     }
   }
 
+  setCurrentCategoryEmpty() {
+    this.currentCategory=undefined
+  }
 }
